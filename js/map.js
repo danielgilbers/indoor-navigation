925324cb3089b366b199dccc4d0d5270c9291634
Daniel Gilbers <danielgilbers@gmail.com>
2024-02-10 18:13:10 +0100

    add map.js

---
+++ js/map.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([50.9058, 6.9348], 17);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);

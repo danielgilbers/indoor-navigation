aa490ceab96dd2b7f9594eb5fd6e372e722da12b
Daniel Gilbers <danielgilbers@gmail.com>
2024-02-19 16:38:07 +0100

    change max zoom level

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 var map = L.map('map').setView([50.9058, 6.9348], 17);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 20,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);

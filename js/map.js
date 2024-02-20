dd16ba25054820e8a711c987209096e92d1e51ec
Daniel Gilbers <danielgilbers@gmail.com>
2024-02-20 18:03:48 +0100

    add zoom function

--- js/map.js
+++ js/map.js
@@ -1,48 +1,63 @@
-var map = L.map('map').setView([50.9058, 6.9348], 17);
+const map = L.map('map').setView([50.9058, 6.9348], 17)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 25,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+  maxZoom: 25,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map)
 
 /**
  * Button definieren
  */
 L.Control.Button = L.Control.extend({
-    onAdd: function (map) {
-        this.container = L.DomUtil.create('div');
-        this.container.innerHTML =
+  onAdd: function (map) {
+    this.container = L.DomUtil.create('div')
+    this.container.innerHTML =
             '<button class="btn btn-primary text-dark rounded-circle p-2 lh-1" type="button">' +
             '<span class="material-symbols-outlined" style="font-variation-settings:\'FILL\' 1; font-size: 30px;">directions</span>' +
-            '</button>';
+            '</button>'
 
-        return this.container;
-    },
+    return this.container
+  },
 
-    onRemove: function (map) {
+  onRemove: function (map) {
 
-    }
-});
+  }
+})
 
 L.control.button = function (opts) {
-    return new L.Control.Button(opts);
+  return new L.Control.Button(opts)
 }
 
-L.control.button({ position: 'bottomright' }).addTo(map);
+L.control.button({ position: 'bottomright' }).addTo(map)
 
 /**
  * GeoJSON Map Layer
  */
-var myStyle = {
-    "color": "#ff0000",
-    "weight": 1,
-    "opacity": 1,
-    "fillOpacity": 0.4
-};
+let toomLayer = L.geoJSON()
+
+const myStyle = {
+  color: '#ff0000',
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.4
+}
+const nvsbl = {
+  opacity: 0,
+  fillOpacity: 0
+}
 
 fetch('./map/map.geojson')
-    .then((response) => response.json())
-    .then((geojsonFeature) => {
-        L.geoJSON(geojsonFeature, {
-            style: myStyle
-        }).addTo(map);
-    });
+  .then((response) => response.json())
+  .then((geojsonFeature) => {
+    toomLayer = L.geoJSON(geojsonFeature, {
+      style: myStyle
+    })
+    toomLayer.addTo(map)
+  })
+
+function onZoomLevelChange (e) {
+  console.log(map.getZoom())
+
+  toomLayer.setStyle(map.getZoom() < 17 ? nvsbl : myStyle)
+}
+
+map.on('zoom', onZoomLevelChange)

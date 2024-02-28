abd81e737dd8ebbdc1f60aa47fdec5070b04e3b5
Daniel Gilbers <danielgilbers@gmail.com>
2024-02-28 15:51:21 +0100

    redesign graphUI

--- js/map.js
+++ js/map.js
@@ -186,12 +186,12 @@ const circle = L.circle([50.9058, 6.9348], {
  */
 L.Control.Search = L.Control.extend({
   onAdd: function () {
-    this.container = L.DomUtil.create('div', 'input-group vw-100 pe-3')
+    this.container = L.DomUtil.create('div', 'input-group vw-100 pe-3 graphUI')
     this.container.innerHTML =
-              '<button class="btn btn-light rounded-start-5 rounded-start-0 lh-1 border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">' +
-              '<span class="material-symbols-outlined" id="addon-wrapping">Menu</span>' +
+              '<button class="btn btn-light rounded-start-5 rounded-end-0 lh-1 border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">' +
+              '<span class="material-symbols-outlined">Menu</span>' +
               '</button>' +
-              '<input type="text" class="graphUI form-control rounded-start-0 rounded-end-5 border-0" placeholder="Suche" aria-label="Search" aria-describedby="addon-wrapping">'
+              '<input type="text" class="form-control rounded-start-0 rounded-end-5 border-0" placeholder="Suche" aria-label="Search" aria-describedby="addon-wrapping">'
 
     return this.container
   }
@@ -205,9 +205,9 @@ new L.Control.Search({ position: 'topleft' }).addTo(map)
  */
 L.Control.QRButton = L.Control.extend({
   onAdd: function () {
-    this.container = L.DomUtil.create('div')
+    this.container = L.DomUtil.create('div', 'graphUI')
     this.container.innerHTML =
-            '<button class="btn btn-primary text-dark rounded-circle p-2 lh-1 graphUI" type="button" id="qrCode">' +
+            '<button class="btn btn-primary text-dark rounded-circle p-2 lh-1" type="button" id="qrCode">' +
             '<span class="material-symbols-outlined" style="font-variation-settings:\'FILL\' 1; font-size: 30px;">qr_code_scanner</span>' +
             '</button>'
 
@@ -222,12 +222,15 @@ new L.Control.QRButton({ position: 'bottomright' }).addTo(map)
  */
 L.Control.GraphButtons = L.Control.extend({
   onAdd: function () {
-    this.container = L.DomUtil.create('div')
+    this.container = L.DomUtil.create('div', 'graphUI d-none')
     this.container.innerHTML =
-            '<button class="graphUI btn btn-primary text-dark rounded-start-5 rounded-end-0 lh-1 d-none" type="button" id="download" data-bs-toggle="modal" data-bs-target="#downloadModal" onclick="createJSON()">' +
+            '<button class="btn btn-light rounded-start-5 rounded-end-0 lh-1 border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">' +
+            '<span class="material-symbols-outlined">Menu</span>' +
+            '</button>' +
+            '<button class="btn btn-light text-dark rounded-0 lh-1 border-0" type="button" id="download" data-bs-toggle="modal" data-bs-target="#downloadModal" onclick="createJSON()">' +
             '<span class="material-symbols-outlined">download</span>' +
             '</button>' +
-            '<button class="graphUI btn btn-primary text-dark rounded-start-0 rounded-end-5 lh-1 d-none" type="button" id="upload" onclick="loadJSON()">' +
+            '<button class="btn btn-light text-dark rounded-start-0 rounded-end-5 lh-1 border-0" type="button" id="upload" onclick="loadJSON()">' +
             '<span class="material-symbols-outlined">upload</span>' +
             '</button>'
 
@@ -235,7 +238,7 @@ L.Control.GraphButtons = L.Control.extend({
   }
 })
 
-new L.Control.GraphButtons({ position: 'bottomright' }).addTo(map)
+new L.Control.GraphButtons({ position: 'topleft' }).addTo(map)
 
 /**
  * GeoJSON Map Layer

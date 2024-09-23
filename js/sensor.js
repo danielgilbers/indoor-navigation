b4041f2083c921a738ef20e8f1510de7f47054e4
Daniel Gilbers <danielgilbers@gmail.com>
2024-09-23 16:04:33 +0200

    reduce array.length

--- js/sensor.js
+++ js/sensor.js
@@ -25,7 +25,7 @@ function handleOrientation (event) {
 const lastValues = []
 
 function addValue (newValue) {
-  if (lastValues.lengh >= 1000) {
+  if (lastValues.lengh >= 100) {
     lastValues.shift()
   }
   lastValues.push(newValue)

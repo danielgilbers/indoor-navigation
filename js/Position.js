17294f2fa565bc67594efa0cba2c8ef392b20552
Daniel Gilbers <danielgilbers@gmail.com>
2024-09-23 12:58:31 +0200

    add standard deviation

---
+++ js/Position.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import { KalmanFilter } from 'https://unpkg.com/kalman-filter@2.3.0/index.js'
+
+export function rotationMatrix (event) {
+  const rotationMatrix = [] // ???
+
+  return rotationMatrix
+}
+
+export function kalmanFilter (event) {
+  const kFilter = new KalmanFilter()
+}

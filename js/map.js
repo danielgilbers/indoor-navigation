70a58f7d0c45d47f794c7cf31d109e54cbb9a0ce
Daniel Gilbers <danielgilbers@gmail.com>
2024-09-24 19:00:12 +0200

    bugfix

--- js/map.js
+++ js/map.js
@@ -269,7 +269,8 @@ function handleOrientation (event) {
   const orientation = 360 - event.webkitCompassHeading
   map.setBearing(orientation + bias)
   motionArray[motionArray.length - 1].push(event.webkitCompassHeading, event.beta, event.gamma)
-  userPosition = L.latLng(calculatePosition(motionArray, userPosition))
+  const newPosition = calculatePosition(motionArray, userPosition)
+  userPosition = L.latLng(newPosition.lat, newPosition.lng)
   circle.setLatLng(userPosition)
   map.flyTo(userPosition, 1)
 }
